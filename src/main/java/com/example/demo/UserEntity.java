ad74f10b52e26cc65dfa8e2b09508c0b5401fb3a
Harivishnu <hariaish88893443@example.com>
2026-08-30 18:56:08 +0530

    fixed error

--- src/main/java/com/example/demo/UserEntity.java
+++ src/main/java/com/example/demo/UserEntity.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +13,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(name = "user_entity")
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserEntity {
